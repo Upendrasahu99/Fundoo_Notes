564f39665a6ae1723121222425a83466c72137a2
Upendra Sahu <upendrasahu1199@gmail.com>
2023-09-13 13:28:55 +0530

    [Upendra] Add: Database_Created_Using_EF_Core_Code_First_Approach

---
+++ RepoLayer/Context/FundooContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepoLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoLayer.Context
+{
+    public class FundooContext : DbContext
+    {
+        public FundooContext(DbContextOptions options) :base(options) { }
+        public DbSet<UserEntity> users { get; set; }
+    }
+}
